1b7607354cee71f1d6f72ba14c10c21152ba6011
KellyZhou223 <kellyzhou223@Gmail.com>
2023-07-09 21:55:11 -0400

    changed header page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Nav from './Nav.js';
 import Row from './Row.js';
-import Scroll from './Scroll.js'
 import About from './About.js'
 
 function Header() {
@@ -9,7 +8,6 @@ function Header() {
         <div className="Header">
             <Nav />
             <Row />
-            <Scroll />
             <About />
         </div> 
         
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,59 @@
 import './App.css';
+import {useRef, useEffect, useState} from 'react';
+
+const images = ["Headshot.png", "Linkedin.jpg", "Mail.jpg", "Github.png"];
+const text = ["I am a...", "Programmer", "Creator", "Problem-Solver"]
+const classes = ["Image1", "Image2", "Image3", "Image4"]
 
 function Row() {
+    const ref1 = useRef();
+    const [isIntersecting, setIntersecting] = useState();
+    useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+        const entry = entries[0];
+        setIntersecting(entry.isIntersecting);
+    });
+    observer.observe(ref1.current);
+    }, [isIntersecting]);
+
+    useEffect(() => {
+    if (isIntersecting) {
+        ref1.current.querySelectorAll("div").forEach(el => {
+        el.classList.add("reveal");
+        });
+    } else {
+        ref1.current.querySelectorAll("div").forEach(el => {
+        el.classList.remove("reveal");
+        });
+    }
+    }, [isIntersecting]);
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            if(currentIndex === images.length - 1) {
+                setCurrentIndex(0);
+            } 
+            else {
+                 setCurrentIndex(currentIndex + 1);
+            }
+        }, 3000)
+        
+        return () => clearInterval(intervalId);
+    }, [currentIndex])
+
     return (
-        <div className="Row">
-            <div class="App-header">
+        <div className="Row" ref={ref1}>
+            <div className="App-header">
                 <p>Hello,</p>
                 <p>I'm Kelly Zhou!</p>
+            </div>
+            <div className="Header-container" >
+                <div className="Dot reveal">
+                    <img className={classes[currentIndex]} src={images[currentIndex]} alt="Char"></img>
+                </div>
+                <p>{text[currentIndex]}</p>
             </div>
         </div>
     )
